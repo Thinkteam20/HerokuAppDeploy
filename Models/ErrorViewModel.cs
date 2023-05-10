b2fab95a11a6972734631efd4268ac2b1f8bb74a
Sangpil <tankteam20@gmail.com>
2023-05-10 11:03:13 +1000

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace HerokuAppDeploy.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
